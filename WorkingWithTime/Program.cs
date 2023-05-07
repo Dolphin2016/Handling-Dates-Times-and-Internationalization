fd9096f70575fd5ec35671e6cceb5e7cf1cc7de7
Dolphin2016 <evgenij55555@gmail.com>
2023-05-07 23:21:47 +0500

    Specifying Date and Time Values

---
+++ WorkingWithTime/Program.cs
@@ -0,0 +1,7 @@
+﻿SectionTitle("Specifying date and time values.");
+
+WriteLine($"DateTime.MinValue:  {DateTime.MinValue}");
+WriteLine($"DateTime.MaxValue:  {DateTime.MaxValue}");
+WriteLine($"DateTime.UnixEpoch: {DateTime.UnixEpoch}");
+WriteLine($"DateTime.Now:       {DateTime.Now}");
+WriteLine($"DateTime.Today:     {DateTime.Today}");
