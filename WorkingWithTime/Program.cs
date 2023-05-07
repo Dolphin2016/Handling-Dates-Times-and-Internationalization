8e69ca6b07a6a5820a024b3888223e1109a0367f
Dolphin2016 <evgenij55555@gmail.com>
2023-05-07 23:37:19 +0500

    Date and Time Calculations

--- WorkingWithTime/Program.cs
+++ WorkingWithTime/Program.cs
@@ -15,3 +15,33 @@ WriteLine($"Christmas (custom format): {xmas:dddd, dd MMMM yyyy}");
 WriteLine($"Christmas is in month {xmas.Month} of the year.");
 WriteLine($"Christmas is day {xmas.DayOfYear} of the year 2024.");
 WriteLine($"Christmas {xmas.Year} is on a {xmas.DayOfWeek}.");
+
+WriteLine();
+WriteLine();
+
+SectionTitle("Date and time calculations");
+
+DateTime beforeXmas = xmas.Subtract(TimeSpan.FromDays(12));
+DateTime afterXmas = xmas.AddDays(12);
+
+// :d means format as short date only without time
+WriteLine($"12 days before Christmas: {beforeXmas:d}");
+WriteLine($"12 days after Christmas: {afterXmas:d}");
+
+TimeSpan untilXmas = xmas - DateTime.Now;
+
+WriteLine($"Now: {DateTime.Now}");
+WriteLine("There are {0} days and {1} hours until Christmas 2024",
+    arg0: untilXmas.Days, arg1: untilXmas.Hours);
+
+WriteLine("There are {0:N0} hours until Christmas 2024.",
+    arg0: untilXmas.TotalHours);
+
+DateTime kidsWakeUp = new(
+    year: 2024, month: 12, day: 25,
+    hour: 6, minute: 30, second: 0);
+
+WriteLine($"Kids wake up: {kidsWakeUp}");
+
+WriteLine("The kids woke me up at {0}",
+    arg0: kidsWakeUp.ToShortTimeString());
