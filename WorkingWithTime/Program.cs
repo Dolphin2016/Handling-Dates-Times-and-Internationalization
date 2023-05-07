d3b88cf2725aec857b8a00595e30f74dca0252eb
Dolphin2016 <evgenij55555@gmail.com>
2023-05-07 23:28:32 +0500

    Formatting Date and Time Values

--- WorkingWithTime/Program.cs
+++ WorkingWithTime/Program.cs
@@ -5,3 +5,13 @@ WriteLine($"DateTime.MaxValue:  {DateTime.MaxValue}");
 WriteLine($"DateTime.UnixEpoch: {DateTime.UnixEpoch}");
 WriteLine($"DateTime.Now:       {DateTime.Now}");
 WriteLine($"DateTime.Today:     {DateTime.Today}");
+
+WriteLine();
+WriteLine();
+
+DateTime xmas = new(year: 2024, month: 12, day: 25);
+WriteLine($"Christmas (default format): {xmas}");
+WriteLine($"Christmas (custom format): {xmas:dddd, dd MMMM yyyy}");
+WriteLine($"Christmas is in month {xmas.Month} of the year.");
+WriteLine($"Christmas is day {xmas.DayOfYear} of the year 2024.");
+WriteLine($"Christmas {xmas.Year} is on a {xmas.DayOfWeek}.");
